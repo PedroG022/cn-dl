932ee924de4424d8c6faccd27e2bc28f5b320c8d
Pedro <55058149+PedroG022@users.noreply.github.com>
2022-10-31 17:28:59 -0300

    Some structural changes Modify the epub file to insert chapter headers

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 from PyPDF2 import PdfFileMerger, PdfFileReader
 from natsort import natsorted
 from sanitize_filename import sanitize
+from zipfile import ZipFile
+import shutil
 
 base_url = ""
 
@@ -94,18 +96,20 @@ def merge(volume: Volume):
     chapter_files = list()
 
     output_epub = f"{volume.complete_name}/{volume.complete_name}.epub"
+    output_epub_old = f"{volume.complete_name}/{volume.complete_name}.old.epub"
     output_pdf = f"{volume.complete_name}/{volume.complete_name}.pdf"
+    output_epub_folder = f"{os.getcwd()}\{volume.complete_name}\epub-output\\"
 
     print(f"Compiling {volume.complete_name}...")
 
-    for file in os.listdir(pdf_path):
-        if isfile(join(pdf_path, file)):
-            chapter_files.append(join(pdf_path, file))
+    for path in os.listdir(pdf_path):
+        if isfile(join(pdf_path, path)):
+            chapter_files.append(join(pdf_path, path))
 
     chapter_files = natsorted(chapter_files)
 
     print("Merging pdfs...")
-
+    
     for chapter in chapter_files:
         chapter_title = chapter[chapter.rfind("/") + 1: chapter.rfind(".")]
         inserted_at = len(pdf_merger.pages)
@@ -116,9 +120,77 @@ def merge(volume: Volume):
 
     print("Converting to EPUB...")
     convert_document(output_pdf, output_epub,
-        ["--no-default-epub-cover", "--toc-title", "Sumário", "--pretty-print", "--epub-inline-toc", "--change-justification", "justify"])
+        ["--no-default-epub-cover", "--toc-title", "Sumário", "--pretty-print", "--epub-inline-toc", "--change-justification", "justify", "--extract-to", output_epub_folder])
 
-    print("Done!")
+    print("Deleting old epub...")
+    os.remove(output_epub)
+
+    print("Merge complete! Now modifiying the volume...")
+
+    htmls = list()
+    anchors = list()
+
+    contents_file = open(join(output_epub_folder, "contents.xhtml"), encoding="utf-8")
+    contents_xhtml = contents_file.read()
+    contents_file.close()
+
+    chapter_header_template_file = open("chapter-title.html", "r", encoding="utf-8")
+    chapter_header_template = chapter_header_template_file.read()
+    chapter_header_template_file.close()
+
+    soup = BeautifulSoup(contents_xhtml, "html.parser")
+    links = soup.find_all("a")
+
+    for link in links:
+        href = link.get("href")
+        anchor = href[href.index("#") + 1: ]
+        anchors.append(anchor)
+
+    for path in os.listdir(output_epub_folder):
+        if path.endswith(".html"):
+            htmls.append(join(output_epub_folder, path))
+
+    for path in htmls:
+        file = open(path, "r", encoding="utf-8")
+        soup = BeautifulSoup(file, "html.parser")
+        
+        for anchor in anchors:
+            objects = soup.select(f"a[id='{anchor}']")
+            if len(objects) > 0:
+                for obj in objects:
+                    number_holder = obj.parent 
+                    title_holder = obj.parent.find_next_sibling('p')
+
+                    number = number_holder.find('b').text
+                    title = title_holder.find('b').text
+
+                    header_copy = chapter_header_template
+                    header_copy = header_copy.replace("chapter_name", title)
+                    header_copy = header_copy.replace("chapter_number", number)
+                    header_copy = header_copy.replace("anchor", anchor)
+
+                    number_holder.replace_with('')
+                    title_holder.replace_with(BeautifulSoup(header_copy, "html.parser"))
+
+        file.close()
+        file = open(path, "w", encoding="utf-8")
+        file.write(str(soup))
+        file.close()
+
+    print("Compressing epub...")
+    zip = ZipFile(output_epub_old, "w")
+
+    for path in os.listdir(output_epub_folder):
+        f = join(output_epub_folder, path)
+        zip.write(f, arcname=os.path.relpath(f, output_epub_folder))
+
+    zip.close()
+
+    print("Re-converting the output file due to cover issues...")
+    convert_document(output_epub_old, output_epub)
+
+    shutil.rmtree(output_epub_folder)
+    os.remove(output_epub_old)
 
 def convert_document(document: str, output: str, options: list() = []) -> str:
     return subprocess.run(['ebook-convert', document, output] + options,  stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT).stdout
